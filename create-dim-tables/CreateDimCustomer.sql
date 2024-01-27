f855994ec1289d44376b85560124eb02154bd360
mau <mau.paunil@gmail.com>
2024-01-27 21:16:44 +0800

    Create script for creating DimCustomer Table

--- create-dim-tables/CreateDimCustomer.sql
+++ create-dim-tables/CreateDimCustomer.sql
@@ -1,4 +1,4 @@
-USE AdventureWorksDW2022
+USE MAU_AdventureWorks2022_DW
 GO
 
 DROP TABLE IF EXISTS dbo.DimCustomer
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS dbo.DimCustomer
 -- Create table for Customer Dimension
 CREATE TABLE dbo.DimCustomer(
 	CustomerID int IDENTITY(1,1) NOT NULL -- Autoincrementing Surrogate Key
-	, AccountNumber nvarchar(10) NOT NULL -- AlternatePK
-	, GeographyID int NOT NULL -- FK 
+	, AccountNumber nvarchar(10) UNIQUE NOT NULL -- Alternate Primary Key
+	, GeographyID int NOT NULL -- Foreign Key
 	, FirstName dbo.NameType 
 	, MiddleName dbo.NameType
 	, LastName dbo.NameType
@@ -15,6 +15,19 @@ CREATE TABLE dbo.DimCustomer(
 	, NameSuffix nvarchar(10)
 	, EmailAddress nvarchar(50) NOT NULL
 	, PhoneNumber nvarchar(25) NOT NULL
+	, BirthDate date NOT NULL
+	, MaritalStatus nchar(1) NOT NULL
+	, YearlyIncome nvarchar(50)
+	, GenderCode nchar(1) NOT NULL
+	, TotalChildren int
+	, NumberChildrenAtHome int
+	, Education nvarchar(25)
+	, Occupation nvarchar(25)
+	, HomeOwnerIndicatior nvarchar(15)
+	, NumberCarsOwned int
+	, CommuteDistance nvarchar(25)
+	, CONSTRAINT PK_DimCustomer_CustomerID PRIMARY KEY CLUSTERED (CustomerID ASC)
+	, CONSTRAINT FK_DimCustomer_GeographyID FOREIGN KEY (GeographyID) REFERENCES MAU_AdventureWorks2022_DW.dbo.DimGeography (GeographyID)
 )
 
 SELECT 
@@ -26,9 +39,20 @@ SELECT
 	, MiddleName = pp.MiddleName
 	, LastName = pp.LastName
 	, FullName = CONCAT(pp.FirstName, ' ', pp.LastName)
-	, pp.Suffix
-	, pea.EmailAddress
-	, ppp.PhoneNumber
+	, NameSuffix = pp.Suffix 
+	, EmailAddress = pea.EmailAddress
+	, PhoneNumber = ppp.PhoneNumber
+	, BirthDate = vcd.BirthDate
+	, MaritalStatus = vcd.MaritalStatus
+	, YearlyIncome = vcd.YearlyIncome
+	, GenderCode = vcd.GenderCode
+	, TotalChildren = vcd.TotalChildren
+	, NumberChildrenAtHome = vcd.NumberChildrenAtHome
+	, Education = vcd.Education
+	, Occupation = vcd.Occupation
+	, HomeOwnerIndicator = vcd.HomeOwnerIndicator
+	, NumberCarsOwned = vcd.NumberCarsOwned
+	, CommuteDistance = vcd.CommuteDistance
 FROM AdventureWorks2022.Sales.Customer sc
 INNER JOIN AdventureWorks2022.Person.Person pp ON pp.BusinessEntityID = sc.PersonID
 INNER JOIN AdventureWorks2022.Person.EmailAddress pea ON pea.BusinessEntityID = sc.PersonID
@@ -42,4 +66,5 @@ INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimGeography ddg
 		AND ddg.StateCode = psp.StateProvinceCode
 		AND ddg.CountryCode	= psp.CountryRegionCode 
 	)
+INNER JOIN AdventureWorks2022.Person.vCustomerDemographics vcd ON vcd.BusinessEntityID = sc.PersonID
 WHERE PersonID IS NOT NULL
