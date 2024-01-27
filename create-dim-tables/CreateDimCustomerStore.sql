f3c8cb3f047ed83b66a464faa26a07e9a18752c8
mau <mau.paunil@gmail.com>
2024-01-28 01:31:45 +0800

    Finalize script for DimCustomerStore creation

--- create-dim-tables/CreateDimCustomerStore.sql
+++ create-dim-tables/CreateDimCustomerStore.sql
@@ -1,20 +1,64 @@
-USE AdventureWorks2022
+USE MAU_AdventureWorks2022_DW
 GO
 
-SELECT 
-	--DISTINCT sc.StoreID
-	 dst.SalesTerritoryID
+DROP TABLE IF EXISTS dbo.DimCustomerStore 
+
+-- Create DimCustomerStore Table
+CREATE TABLE dbo.DimCustomerStore(
+	StoreID int IDENTITY(1,1) -- Autoincrementing surrogate key
+	, SalesTerritoryID int NOT NULL -- Foreign key to DimSalesTerritory table
+	, SalesPersonID int NOT NULL  -- Foreign key to DimSalesPerson table
+	, StoreName dbo.NameType
+	, AnnualSales int NOT NULL
+	, AnnualRevenue int NOT NULL
+	, BankName nvarchar(50)
+	, BusinessType nvarchar(5)
+	, YearOpened int
+	, Specialty nvarchar(50)
+	, SquareFeet int
+	, Brands nvarchar(30)
+	, Internet nvarchar(30)
+	, NumberEmployees int
+	, DateCreated date
+	, DateModified date
+	, CONSTRAINT PK_DimCustomerStore_StoreID PRIMARY KEY CLUSTERED (StoreID ASC)
+	, CONSTRAINT FK_DimCustomerStore_DimSalesTerritory FOREIGN KEY (SalesTerritoryID)
+		REFERENCES MAU_AdventureWorks2022_DW.dbo.DimSalesTerritory(SalesTerritoryID)
+	, CONSTRAINT FK_DimCustomerStore_DimSalesPerson FOREIGN KEY (SalesPersonID)
+		REFERENCES MAU_AdventureWorks2022_DW.dbo.DimSalesPerson(SalesPersonID)
+)
+
+
+-- Populate DimCustomerStore Table with data from OLTP database
+INSERT INTO dbo.DimCustomerStore
+SELECT DISTINCT dst.SalesTerritoryID
 	, dsp.SalesPersonID
 	, StoreName = ss.[Name]
-	, ss.*
+	, AnnualSales = vsd.AnnualSales
+	, AnnualRevenue = vsd.AnnualRevenue 
+	, BankName = vsd.BankName
+	, BusinessType = vsd.BusinessType
+	, YearOpened = vsd.YearOpened
+	, Specialty = vsd.Specialty
+	, SquareFeet = vsd.SquareFeet
+	, Brands = vsd.Brands
+	, Internet = vsd.Internet
+	, NumberEmployees = vsd.NumberEmployees
+	, DateCreated = GETDATE()
+	, DateModified = GETDATE()
 FROM AdventureWorks2022.Sales.Customer sc
 INNER JOIN AdventureWorks2022.Sales.Store ss ON ss.BusinessEntityID = sc.StoreID
 INNER JOIN AdventureWorks2022.Sales.SalesPerson ssp ON ssp.BusinessEntityID = ss.SalesPersonID
 INNER JOIN AdventureWorks2022.Person.Person pp ON pp.BusinessEntityID = ssp.BusinessEntityID
-INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimSalesPerson dsp ON
+LEFT JOIN MAU_AdventureWorks2022_DW.dbo.DimSalesPerson dsp ON
 	dsp.FirstName = pp.FirstName
-	AND dsp.MiddleName = pp.MiddleName
 	AND dsp.LastName = pp.LastName
 INNER JOIN AdventureWorks2022.Sales.SalesTerritory sst ON sst.TerritoryID = sc.TerritoryID
-INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimSalesTerritory dst ON dst.SalesTerritory = sst.[Name]
-WHERE StoreID IS NOT NULL
+LEFT JOIN MAU_AdventureWorks2022_DW.dbo.DimSalesTerritory dst ON dst.SalesTerritory = sst.[Name]
+LEFT JOIN AdventureWorks2022.Sales.vStoreDemographics vsd ON vsd.BusinessEntityID = ss.BusinessEntityID
+WHERE sc.StoreID IS NOT NULL
+
+
+-- Show sample data
+SELECT TOP(10) * FROM dbo.DimCustomerStore
+
