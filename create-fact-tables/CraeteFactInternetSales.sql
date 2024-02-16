14a99a3096cd09ba63f86ca3ffbdfbb65d8255d1
mau <mau.paunil@gmail.com>
2024-02-16 23:16:36 +0800

    Finalized script for Internet sales fact

--- create-fact-tables/CraeteFactInternetSales.sql
+++ create-fact-tables/CraeteFactInternetSales.sql
@@ -1,16 +1,12 @@
 USE MAU_AdventureWorks2022_DW
 GO
 
---SELECT * FROM AdventureWorks2022.Sales.SalesOrderDetail
---WHERE UnitPriceDiscount <> 0
+DROP TABLE IF EXISTS dbo.FactInternetSales
 
---SELECT * FROM AdventureWorks2022.Sales.SalesOrderHeader
-
-
-;WITH ConvertedPrices AS  (
+;WITH ConvertedPrices AS  ( -- Intermediate table to compute for converted prices
 SELECT 
-	ssoh.SalesOrderID 
-	, ssod.SalesOrderDetailID
+	SalesOrderID = ssoh.SalesOrderID 
+	, SalesOrderDetailID = ssod.SalesOrderDetailID
 	, ConvertedUnitPrice = 
 		CASE WHEN scr.CurrencyRateID IS NULL THEN ssod.UnitPrice -- When CurrencyRateID is NULL, Price is already in USD. No need to convert.
 			 ELSE ssod.UnitPrice / scr.AverageRate
@@ -19,46 +15,54 @@ SELECT
 		CASE WHEN scr.CurrencyRateID IS NULL THEN ssod.LineTotal -- When CurrencyRateID is NULL, Price is already in USD. No need to convert.
 			ELSE ssod.LineTotal / scr.AverageRate
 		END
-	, ssod.LineTotal
-	, CurrencyUniqueID = 
-		CASE WHEN ddc.CurrencyUniqueID IS NULL THEN 100
-			 ELSE ddc.CurrencyUniqueID
-		END
+	, CurrencyUniqueID = 100 -- ALL costs are converted to USD
 	, ConvertedTaxAmount = 
-		CASE WHEN scr.CurrencyRateID IS NULL THEN (ssoh.TaxAmt)*(ssod.LineTotal/ssoh.SubTotal)
+		CASE WHEN scr.CurrencyRateID IS NULL THEN (ssoh.TaxAmt)*(ssod.LineTotal/ssoh.SubTotal)  -- When CurrencyRateID is NULL, Price is already in USD. No need to convert.
 			 ELSE (ssoh.TaxAmt/scr.AverageRate)*(ssod.LineTotal/ssoh.SubTotal)
 		END
 	, ConvertedFreight = 
-		CASE WHEN scr.CurrencyRateID IS NULL THEN (ssoh.Freight)*(ssod.LineTotal/ssoh.SubTotal)
+		CASE WHEN scr.CurrencyRateID IS NULL THEN (ssoh.Freight)*(ssod.LineTotal/ssoh.SubTotal)  -- When CurrencyRateID is NULL, Price is already in USD. No need to convert.
 			 ELSE (ssoh.Freight/scr.AverageRate)*(ssod.LineTotal/ssoh.SubTotal)
+		END
+	, ConvertedProductCost = 
+		CASE WHEN scr.CurrencyRateID IS NULL THEN pp.StandardCost
+			 ELSE pp.StandardCost/scr.AverageRate
 		END
 FROM AdventureWorks2022.Sales.SalesOrderDetail ssod
 INNER JOIN AdventureWorks2022.Sales.SalesOrderHeader ssoh ON ssoh.SalesOrderID = ssod.SalesOrderID
 LEFT JOIN AdventureWorks2022.Sales.CurrencyRate scr ON scr.CurrencyRateID = ssoh.CurrencyRateID
 LEFT JOIN MAU_AdventureWorks2022_DW.dbo.DimCurrency ddc ON ddc.CurrencyCode = scr.ToCurrencyCode
+LEFT JOIN AdventureWorks2022.Production.Product pp ON pp.ProductID = ssod.ProductID
+WHERE ssoh.OnlineOrderFlag = 1 -- get only Sales data from Internet
 )
 
 SELECT 
 	OrderID = ssoh.SalesOrderNumber -- Degenerate Key 
+	, SalesOrderDetailID = cp.SalesOrderDetailID -- DegenerateKey
 	, PONumber = CASE WHEN ssoh.PurchaseOrderNumber IS NULL THEN 'NO PO Number'
 					  ELSE ssoh.PurchaseOrderNumber
 				 END
-	, ssoh.OnlineOrderFlag
-	, CustomerUniqueID = 1
-	, ProductUniqueID = ddp.ProductUniqueID
-	, CurrencyUniqueID = cp.CurrencyUniqueID
-	, PromoUniqueID = ddp2.PromoUniqueID
+	, OrderDate = ddd1.DateKey
+	, DueDate = ddd2.DateKey
+	, ShipDate = ddd3.DateKey
+	, CustomerKey = ddc.CustomerUniqueID
+	, ProductKey = ddp.ProductUniqueID
+	, CurrencyKey = cp.CurrencyUniqueID
+	, PromoKey = ddp2.PromoUniqueID
 	, UnitPrice = cp.ConvertedUnitPrice
 	, Quantity = ssod.OrderQty
+	, LinePrice = cp.ConvertedUnitPrice*ssod.OrderQty
 	, Discount = ssod.UnitPriceDiscount
 	, UnitPriceDiscount = ssod.UnitPriceDiscount * cp.ConvertedUnitPrice
-	, TotalLineDiscount =  ssod.UnitPriceDiscount * cp.ConvertedUnitPrice * ssod.OrderQty
+	, LineDiscount =  ssod.UnitPriceDiscount * cp.ConvertedUnitPrice * ssod.OrderQty
 	, SalesAmount = cp.ConvertedLineTotal
 	, TaxAmount = cp.ConvertedTaxAmount
 	, Freight = cp.ConvertedFreight
 	, TotalDue = cp.ConvertedLineTotal + cp.ConvertedTaxAmount + cp.ConvertedFreight
-	, sc.*
-	, pp2.*
+	, UnitCost = cp.ConvertedProductCost
+	, LineCost = cp.ConvertedProductCost * ssod.OrderQty
+	, NetSalesAmount =  cp.ConvertedLineTotal - cp.ConvertedProductCost * ssod.OrderQty
+INTO dbo.FactInternetSales -- automatically creates FactInternetSales table
 FROM ConvertedPrices cp 
 INNER JOIN AdventureWorks2022.Sales.SalesOrderHeader ssoh ON ssoh.SalesOrderID = cp.SalesOrderID
 INNER JOIN AdventureWorks2022.Sales.SalesOrderDetail ssod ON ssod.SalesOrderDetailID = cp.SalesOrderDetailID
@@ -66,6 +70,12 @@ INNER JOIN AdventureWorks2022.Production.Product pp ON pp.ProductID = ssod.Produ
 INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimProduct ddp ON ddp.ProductCode = pp.ProductNumber
 INNER JOIN AdventureWorks2022.Sales.SpecialOffer sso ON sso.SpecialOfferID	= ssod.SpecialOfferID
 INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimPromo ddp2 ON ddp2.Promo = sso.[Description]
-INNER JOIN AdventureWorks2022.Sales.Customer sc ON ssoh.CustomerID = sc.CustomerID
-LEFT JOIN AdventureWorks2022.Person.Person pp2 ON pp2.BusinessEntityID = sc.PersonID
+INNER JOIN AdventureWorks2022.Sales.Customer sc ON sc.CustomerID = ssoh.CustomerID 
+INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimCustomer ddc ON ddc.AccountNumber = sc.AccountNumber
+INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimDate ddd1 ON ddd1.[Date] = ssoh.OrderDate
+INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimDate ddd2 ON ddd2.[Date] = ssoh.DueDate
+INNER JOIN MAU_AdventureWorks2022_DW.dbo.DimDate ddd3 ON ddd3.[Date] = ssoh.ShipDate
+
+-- View Sample Date
+SELECT TOP(10) * FROM dbo.FactInternetSales
 
